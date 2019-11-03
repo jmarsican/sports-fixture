ab658f124aa98bf1168d988cdcbcef9f21ffbfcf
javier <jmarsican@gmail.com>
2019-11-03 18:59:14 -0300

    implement tabs

--- app/src/main/java/com/javiermarsicano/sportsfixture/views/main/MainActivity.kt
+++ app/src/main/java/com/javiermarsicano/sportsfixture/views/main/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.javiermarsicano.sportsfixture.views.main
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import com.javiermarsicano.sportsfixture.R
 import com.javiermarsicano.sportsfixture.common.mvp.BaseMVPActivity
-import com.javiermarsicano.sportsfixture.views.fixtureslist.FixturesFragment
 import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.ref.WeakReference
 
@@ -14,16 +14,25 @@ class MainActivity : BaseMVPActivity<MainView, MainPresenter>(), MainView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Add list fragment if this is first creation
-        if (savedInstanceState == null) {
-            val fragment = FixturesFragment.newInstance()
-
-            supportFragmentManager.beginTransaction()
-                    .add(R.id.fragment_container, fragment)
-                    .commit()
-        }
-
         setLoaderView(loaderView)
+
+        tab_layout.addTab(tab_layout.newTab().setText(getString(R.string.fixtures)))
+        tab_layout.addTab(tab_layout.newTab().setText(getString(R.string.result)))
+        val tabsAdapter = TabsAdapter(supportFragmentManager)
+        view_pager.adapter = tabsAdapter
+        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+        tab_layout.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                view_pager.setCurrentItem(tab.position)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+        })
+
     }
 
 }
---
+++ app/src/main/java/com/javiermarsicano/sportsfixture/views/main/TabsAdapter.kt
@@ -0,0 +1,15 @@
+package com.javiermarsicano.sportsfixture.views.main
+
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import com.javiermarsicano.sportsfixture.views.fixtureslist.FixturesFragment
+
+class TabsAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm) {
+    override fun getCount(): Int = 2
+
+    override fun getItem(position: Int) =
+        when (position) {
+            0 -> FixturesFragment()
+            else -> FixturesFragment()
+        }
+}
